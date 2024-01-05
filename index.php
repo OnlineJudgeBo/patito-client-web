f164723fcc2ac2a735529699319a445d384be78f
Samuel Loza <samuel.loza26@gmail.com>
2024-01-05 13:04:33 -0400

    Cleanup and Refactor folders

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
-require_once('./include/cache_start.php');
 require_once('./include/db_info.inc.php');
-require_once('./include/setlang.php');
 require_once ("./include/const.inc.php");
 
 $view_title = "Bienvenido al Juez de la Carrera de Informatica - UMSA";
