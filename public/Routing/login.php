5a58ef46d20b2ddae468945e6e9c49e13a326b6d
Samuel Loza <samuel.loza26@gmail.com>
2024-02-15 20:20:03 -0400

    fix login problem

--- public/Routing/login.php
+++ public/Routing/login.php
@@ -4,7 +4,7 @@ use PatitoOnlineJudge\Presentation\Controller\LoginController;
 
 require_once __DIR__ . '/container.php';
 
-$constListProblemController = $container->get(LoginController::class);
+$loginController = $container->get(LoginController::class);
 
 if (isset($_POST["username"]) && isset($_POST["password"])) {
     $loginController->login($_POST["username"], $_POST["password"]);
