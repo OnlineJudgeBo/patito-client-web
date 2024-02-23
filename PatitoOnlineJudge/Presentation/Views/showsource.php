a371147b78d6fe111a3f037f0e6395bd697f3f1d
Samuel Loza <samuel.loza26@gmail.com>
2024-02-23 13:42:51 -0400

    fix: problems with the visualization of the code in showcode page #48

--- PatitoOnlineJudge/Presentation/Views/showsource.php
+++ PatitoOnlineJudge/Presentation/Views/showsource.php
@@ -9,7 +9,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="./assets/highlight/highlight.min.js"></script>
-    <link rel="stylesheet" href="./assets/highlight/styles/windows-95.css">
+    <link rel="stylesheet" href="./assets/highlight/styles/nnfx-light.min.css">
     <script src="//cdnjs.cloudflare.com/ajax/libs/highlightjs-line-numbers.js/2.8.0/highlightjs-line-numbers.min.js"></script>
 
     <link rel="stylesheet" href="./assets/base.css">
@@ -54,23 +54,40 @@
 
     <?php require_once "oj-header.php" ?>
     <?php require __DIR__ . "/Modules/Utils.php"; ?>
+    <?php
+    include(__DIR__ . "/../../../Legacy/Include/const.inc.php");
+    $language = "";
+    if (str_contains($language_name[$sourceDetail["language"]], "Python") === true) {
+        $language = "python";
+    } elseif (str_contains($language_name[$sourceDetail["language"]], "C") === true) {
+        $language = "cpp";
+    } elseif (str_contains($language_name[$sourceDetail["language"]], "Java") === true) {
+        $language = "java";
+    }
+    ?>
     <main class="w-full">
         <div class="flex flex-col items-center">
             <div class="rounded-lg border bg-card text-card-foreground shadow-sm w-full">
                 <div class="p-1">
                     <div class="relative w-full h-full">
-                        <pre ><code class="code"><?php echo htmlspecialchars($sourceDetail["source"]); ?>
+                        <pre><code class="language-<?php echo $language ?>"><?php echo htmlspecialchars($sourceDetail["source"]); ?>
+
+
 
 <?php
-echo "**************************************************************\n";
 include(__DIR__ . "/../../../Legacy/Include/const.inc.php");
-echo "Problema: ".$sourceDetail["problem_id"]."\nUsuario: ".$sourceDetail["user_id"]."\n";
-echo "Lenguaje: ".$language_name[$sourceDetail["language"]]."\nResult: ".$judge_result[$sourceDetail["result"]]."\n";
-if ($sourceDetail["result"] == 4){
-    echo "Time:".$sourceDetail["time"]." ms\n";
-    echo "Memory:".$sourceDetail["memory"]." kb\n";
+$comment = "/";
+if (str_contains($language_name[$sourceDetail["language"]], "Python") === true) {
+    $comment = "#";
 }
-echo "****************************************************************\n";
+echo $comment . "**************************************************************$comment\n";
+echo "Problema: " . $sourceDetail["problem_id"] . "\nUsuario: " . $sourceDetail["user_id"] . "\n";
+echo "Lenguaje: " . $language_name[$sourceDetail["language"]] . "\nResult: " . $judge_result[$sourceDetail["result"]] . "\n";
+if ($sourceDetail["result"] == 4) {
+    echo "Time:" . $sourceDetail["time"] . " ms\n";
+    echo "Memory:" . $sourceDetail["memory"] . " kb\n";
+}
+echo $comment . "**************************************************************$comment\n";
 ?>
                     </code></pre>
                     </div>
@@ -82,10 +99,10 @@ echo "****************************************************************\n";
 
     <?php require_once "oj-footer.php" ?>
     <script type="text/javascript">
+        hljs.initLineNumbersOnLoad();
+        hljs.highlightAll();
         document.querySelectorAll('code').forEach(el => {
-            // then highlight each
             hljs.highlightElement(el);
-            hljs.initLineNumbersOnLoad();
         });
     </script>
 </body>
