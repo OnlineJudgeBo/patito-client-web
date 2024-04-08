806c3ca383ab1cc10c2f477b8b55f15a2edee3b9
Samuel Loza <samuel.loza26@gmail.com>
2024-04-08 12:35:48 -0400

    fix send recovery mail

--- PatitoOnlineJudge/Core/Application/Services/MailService.php
+++ PatitoOnlineJudge/Core/Application/Services/MailService.php
@@ -29,7 +29,25 @@ class MailService implements IMailService
         $this->mailer->Password = "qmrtolnhjblhijau";
         $this->mailer->AddAddress($email);
         $this->mailer->Subject = "Juez Virtual";
-        $this->mailer->Body = "Para reiniciar su contraseña, haga clic en el siguiente link <br> : <a href='https://jv.umsa.bo/oj/updatepassword.php?token=" . urlencode($encodePassword) . "'>Cambiar clave</a>";
+        $this->mailer->Body = $this->buildMessage($encodePassword);
         $this->mailer->Send();
+    }
+
+    private function buildMessage($encodePassword)
+    {
+        $userId = $_SESSION['user_id'];
+        $sms = "Hola,<br>
+        Solicitaste restablecer tu contraseña. A continuación se te enviará el nombre de usuario de tu cuenta:
+        <table>
+            <tr>
+                <td>Nombre de usuario:</td>
+                <td>{$userId}</td>
+            </tr>
+        </table>
+        <br>
+        Para restablecer tu contraseña, haz clic en el siguiente enlace: <a href='https://jv.umsa.bo/oj/updatepassword.php?token=" . urlencode($encodePassword) . "'>Cambiar contraseña</a>
+        <br> o copia el siguiente enlace y pégalo en tu navegador: 
+        https://jv.umsa.bo/oj/updatepassword.php?token=" . urlencode($encodePassword);
+        return $sms;
     }
 }
