85ded02e3f1db8c838c9fc2918b898b9f552acd9
Samuel Loza <samuel.loza26@gmail.com>
2024-04-14 16:49:13 -0400

    Fix util to generate passwordHash

--- public/Routing/spi.php
+++ public/Routing/spi.php
@@ -2,8 +2,11 @@
 
 echo generatePasswordHash($_GET["spi"]);
 
-function generatePasswordHash($password)
+function generatePasswordHash($password, $isMd5 = false)
 {
+    if (!$isMd5) {
+        $password = md5($password);
+    }
     $salt = sha1(rand());
     $salt = substr($salt, 0, 4);
     $hash = base64_encode(sha1($password . $salt, true) . $salt);
