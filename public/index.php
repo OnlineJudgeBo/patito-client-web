a08913361a20f44730c3fd296b7aa985a8f8e4cf
Samuel Loza <samuel.loza26@gmail.com>
2024-04-17 22:32:50 -0400

    Update index.php

--- public/index.php
+++ public/index.php
@@ -17,7 +17,7 @@ error_reporting(E_ALL);*/
 try {
     $router = new Router();
     $authMiddleware = new AuthMiddleware();
-    $prefix = "/oj";
+    $prefix = "";
 
     $databaseConnector = new DatabaseConnector();
     $logRepository = new LogRepository($databaseConnector);
