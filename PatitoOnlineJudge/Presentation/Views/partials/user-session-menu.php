382207949eddfa11d549946a1b44af580d2f6dbd
Samuel Loza <samuel.loza26@gmail.com>
2024-04-21 17:20:09 -0400

    Change user roles

--- PatitoOnlineJudge/Presentation/Views/partials/user-session-menu.php
+++ PatitoOnlineJudge/Presentation/Views/partials/user-session-menu.php
@@ -4,12 +4,16 @@ if (isset($_SESSION['user_id'])) {
 ?>
     <a href="modifypage.php" class="<?php echo $className; ?>">Editar Usuario</a>
     <a href="logout.php" class="<?php echo $className; ?>">Salir</a>
-<?php
-   if (isset($_SESSION["administrator"]) && $_SESSION["administrator"] == "administrator" || isset($_SESSION["contest_creator"])) {
-    echo "<a href='https://jv.umsa.bo/admin/' class='".$className."' target='_blank' >Administrar</a>";
-   }
+    <?php
+    if (
+        isset($_SESSION["Administrador"]) && $_SESSION["Administrador"] == "Administrador" ||
+        isset($_SESSION["Docente"])       && $_SESSION["Docente"]       == "Docente"  ||
+        isset($_SESSION["Auxiliar"])      && $_SESSION["Auxiliar"]      == "Auxiliar" ||
+        isset($_SESSION["contest_creator"])) {
+        echo "<a href='https://jv.umsa.bo/admin/' class='" . $className . "' target='_blank' >Administrar</a>";
+    }
 } else {
-?>
+    ?>
     <a href="login.php" class="<?php echo $className; ?>">Iniciar sesión</a>
     <a href="registerpage.php" class="<?php echo $className; ?>">Registrarse</a>
 <?php
