779aee2317cde23b06d17995114a68d7c8a01057
Samuel Loza <samuel.loza26@gmail.com>
2024-05-11 02:29:22 -0400

    Remove prefix

--- public/index.php
+++ public/index.php
@@ -19,7 +19,7 @@ error_reporting(E_ALL);
 try {
     $router = new Router();
     $authMiddleware = new AuthMiddleware();
-    $prefix = "/";
+    $prefix = "";
 
     $databaseConnector = new DatabaseConnector();
     $logRepository = new LogRepository($databaseConnector);
