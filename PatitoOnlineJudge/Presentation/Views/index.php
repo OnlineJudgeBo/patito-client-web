d52bb91db3de8e18556e7a1b3e0aa68546605d83
Samuel Loza <samuel.loza26@gmail.com>
2024-05-16 22:45:05 -0400

    Add Google Search verification

--- PatitoOnlineJudge/Presentation/Views/index.php
+++ PatitoOnlineJudge/Presentation/Views/index.php
@@ -4,6 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="google-site-verification" content="7lqeypTs6VScmVRg6rQJA8_C-orO482PL_4vVbYjLc0" />
   <title><?php echo $title ?></title>
   <link href='https://fonts.googleapis.com/css?family=Capriola' rel='stylesheet'>
   <script src="https://cdn.tailwindcss.com"></script>
