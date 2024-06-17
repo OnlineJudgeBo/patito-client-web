27f1e1e694a3731e9d5c8a52ed8436a24794341f
Samuel Loza <samuel.loza26@gmail.com>
2024-06-17 19:26:37 -0400

    Fix typo error

--- PatitoOnlineJudge/Presentation/Views/login.php
+++ PatitoOnlineJudge/Presentation/Views/login.php
@@ -20,7 +20,7 @@
 
         <div class="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
             <div class="mx-auto max-w-2xl text-center">
-                <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Inicio de session</h2>
+                <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Inicio de Sesión</h2>
             </div>
             <form action="login.php" method="POST" class="mx-auto mt-16 max-w-xl sm:mt-20">
                 <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
