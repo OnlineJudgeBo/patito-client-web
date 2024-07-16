f4fd45c8adc389df4826fbe227eb098bd035ca9b
Samuel Loza <samuel.loza26@gmail.com>
2024-07-16 06:20:56 -0400

    fix numeration

--- PatitoOnlineJudge/Presentation/Views/patito/contestProblemList.php
+++ PatitoOnlineJudge/Presentation/Views/patito/contestProblemList.php
@@ -94,7 +94,7 @@
                                         echo "Y";
                                     }
                                     echo "</td>";
-                                    echo '<td class="p-1 text-center align-middle">' . $value["pid"] . " " . ' ' . chr($letter) . " " . $xtra_letter . '</td>';
+                                    echo '<td class="p-1 text-center align-middle">' . $value["pid"] . " " . ' ' . chr($letter) . "" . $xtra_letter . '</td>';
                                     $problemUrl = sprintf("problem.php?cid=%d&pid=%d", $cid, $value["pnum"]);
                                     if (isset($cType) && $cType == "official_contest") {
                                         $problemUrl = sprintf("problem.php?cid=%d&pid=%d&type=%s", $cid, $value["pnum"], $cType);
