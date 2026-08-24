a24cd8dca9b74e698155062bcc9065331c14e14c
Samuel Loza <samuel.loza26@gmail.com>
2026-08-24 09:41:16 -0400

    fix: avoid duplicate Utils.php require causing fatal error in showsource and resultAnswer

--- PatitoOnlineJudge/Presentation/Views/patito/resultAnswer.php
+++ PatitoOnlineJudge/Presentation/Views/patito/resultAnswer.php
@@ -18,7 +18,7 @@
 <body class="flex flex-col h-full">
 
     <?php require_once "oj-header.php" ?>
-    <?php require __DIR__ . "/Modules/Utils.php"; ?>
+    <?php require_once __DIR__ . "/Modules/Utils.php"; ?>
     <main class="w-full">
         <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
             <div class="md:flex">
